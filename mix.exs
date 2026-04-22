eaa61b64b96ea803df78142db62aa896696faf02
Tyler Eon <tyler@eon.dev>
2026-04-21 23:47:32 -0700

    Prepare project for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,25 +1,41 @@
 defmodule Multibase.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/tyler-eon/multibase"
+
   def project do
     [
       app: :multibase,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       description: "An Elixir implementation of the multibase specification for self-describing base encodings.",
       package: package(),
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      source_url: @source_url
     ]
   end
 
   def package do
     [
-      name: "multibase",
+      name: "multibase_ex",
       maintainers: ["Tyler Eon"],
-      licenses: ["MIT"],
+      licenses: ["MPL-2.0"],
       links: %{
-        "GitHub" => "https://github.com/tyler-eon/multibase"
-      }
+        "GitHub" => @source_url
+      },
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      name: "Multibase",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md", "LICENSE"]
     ]
   end
 
